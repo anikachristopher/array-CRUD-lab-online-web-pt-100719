4934b79e70bb0a005f308ad96f2781d0ceda0f6b
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-13 17:26:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,13 @@ def create_an_array
 end
 
  def add_element_to_end_of_array  
-   add_element_to_end_of_array  = ["red", "yellow", "blue", "green"]
+  add_element_to_end_of_array  = ["red", "yellow", "blue", "green"]
   add_element_to_end_of_array << "white"
  end 
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("purple")
+  add_element_to_start_of_array  = ["red", "yellow", "blue", "green"]
+  add_element_to_end_of_array.unshift("purple")
 end
 
 def remove_element_from_end_of_array(array)
