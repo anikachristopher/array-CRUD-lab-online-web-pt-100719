2b5e1de25fe4f59e40f1ac9d023ebddd8f8411f7
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-13 17:06:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array = ["red", "yellow", "blue", "green"]
+  add_element_to_end_of_array  = ["red", "yellow", "blue", "green"]
   add_element_to_end_of_array << "white"
-end
+  
 
 def add_element_to_start_of_array(array, element)
   create_an_array.unshift("purple")
